b6c61ff1bde57cc24c62dfd00509174c605dbe68
Şahin Hürcan <sahin@sahinhurcan.com.tr>
2025-01-27 16:34:30 +0300

    Refactor Firebase integration and remove vendor dependencies
    Refactor the project to use CDN links for Firebase and Ionic libraries, and update the code to use Firebase service directly.
    
    * **index.html**
      - Replace `lib/firebase/firebase.js` script tag with CDN link to Firebase.
      - Replace `lib/ionic/js/ionic.bundle.js` script tag with CDN link to Ionic.
    
    * **js/_controller.js**
      - Remove `firebase` dependency from the `game` controller.
      - Update `fn.getScore` function to use Firebase service directly.
      - Update `fn.setScore` function to use Firebase service directly.
      - Update `fn.setUsername` function to use Firebase service directly.
    
    * **js/_service.js**
      - Remove `firebase` factory.
      - Update `AdMob` factory to remove dependency on `firebase` factory.
    
    * **js/app.js**
      - Remove Firebase configuration and initialization code.
      - Remove Firebase network controller code.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/sahinhurcan/GameOfDots?shareId=XXXX-XXXX-XXXX-XXXX).

--- js/app.js
+++ js/app.js
@@ -12,24 +12,4 @@ angular.module('starter', ['ionic', 'game.services', 'game.controllers', 'game.d
                 StatusBar.styleDefault();
             }
         });
-
-        //  FIREBASE CONFIG & NETWORK CONTROLLER
-        var config = {
-            apiKey: "AIzaSyBYs4jHczjQ2H78Seih_AqX_5F1fioVEWA",
-            authDomain: "dots-game-7621e.firebaseapp.com",
-            databaseURL: "https://dots-game-7621e.firebaseio.com",
-            projectId: "dots-game-7621e",
-            storageBucket: "dots-game-7621e.appspot.com",
-            messagingSenderId: "515143376174"
-        };
-        firebase.initializeApp(config);
-        firebase.database().ref(".info/connected").on("value", function (snapshot) {
-            var resp = snapshot.val();
-            if (resp === true) {
-                $rootScope.network = true;
-                AdMob.init();
-            } else {
-                $rootScope.network = false;
-            }
-        });
     });
